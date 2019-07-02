4187f93b72f805faaa166767503883054490212d
hexu <hexu_1985@sina.com>
2019-07-02 10:38:47 +0800

    add samples/weight/sample_sparse_multi_graph1.cpp

---
+++ samples/weight/sample_sparse_multi_graph1.cpp
@@ -0,0 +1,55 @@
+/** \example sample_sparse_multi_graph1.cpp
+ * This is an example of how to use the SparseMultiGraph class.
+ */
+#include <vector>
+#include "mini_graph.hpp"
+
+using namespace std;
+using namespace MiniGraph::weight;
+
+int main()
+{
+    int vertexNumber = 8;
+    vector<Edge> edges = {
+        {0,6, .51}, 
+        {0,1, .32}, 
+        {0,2, .29}, 
+        {4,3, .34}, 
+        {5,3, .18}, 
+        {7,4, .46},
+        {5,4, .40}, 
+        {0,5, .60}, 
+        {6,4, .51}, 
+        {7,0, .31}, 
+        {7,6, .25},
+        {7,1, .21} 
+    }; // graph1.jpg
+
+    SparseMultiGraph graph(vertexNumber, false);  // directed false
+
+    cout << "insert edges\n";
+    for (auto &edge: edges)
+        graph.insert(&edge);
+
+    // show edges
+    cout << graph.edgeCount() << " edges in graph" << endl;
+
+    // show adjLists
+    cout << "-----------------show adjLists of Graph--------------------\n";
+    GraphPrinter::showAdjLists(graph);
+    cout << endl;
+
+    cout << "remove edges\n";
+    for (auto edge: MiniGraph::weight::edges(graph))
+        graph.remove(edge);
+
+    // show edges
+    cout << graph.edgeCount() << " edges in graph" << endl;
+
+    // show adjLists
+    cout << "-----------------show adjLists of Graph--------------------\n";
+    GraphPrinter::showAdjLists(graph);
+    cout << endl;
+
+    return 0;
+}
