545edbbdbddf28411ec3fa30fe4f236426516962
hexu <hexu_1985@sina.com>
2019-10-21 14:01:24 +0800

    add samples/normal/undigraph_cc/sample_undigraph_cc2.cpp

---
+++ samples/normal/undigraph_cc/drive_sample_undigraph_cc2.sh
@@ -0,0 +1,3 @@
+make sample_undigraph_cc2
+./sample_undigraph_cc2
+dot sample_undigraph_cc2.dot -T png -o sample_undigraph_cc2.png
---
+++ samples/normal/undigraph_cc/sample_undigraph_cc2.cpp
@@ -0,0 +1,59 @@
+/** \example normal/undigraph_cc/sample_undigraph_cc2.cpp
+ * This is an example of how to use the UndigraphCC class.
+ */
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <map>
+#include <sstream>
+#include "mini_graph.hpp"
+
+using namespace std;
+using namespace mini_graph::normal;
+
+int main()
+{
+    int vertexNumber = 13;
+    vector<Edge> edges = {
+        {10,6}, {10,7}, {10,2}, {7,2}, {2,12}, {6,12}, {3,12}, {5,12}, 
+        {11,8}, 
+        {9,0}, {9,1}, {0,1}, {1,4}}; // graph2.jpg
+
+    SparseMultiGraph graph(vertexNumber, false);  // directed false
+    for (auto edge: edges)
+        graph.insert(edge);
+
+    UndigraphCC<SparseMultiGraph> cc(graph);
+    cc.search();
+
+    // 将属于同一连通分量的顶点索引放在一起
+    std::map<int, std::vector<int>> cc2v_map;
+    for (int v = 0; v < graph.vertexCount(); v++)
+        cc2v_map[cc.getCCNumber(v)].push_back(v);
+
+    // 将一个连通分量作为一个subgraph
+    GraphvizWriter::AttributesForLambda attributes;
+    attributes.get_additional_attributes = 
+        [&]() -> std::string {
+            std::ostringstream os;
+            for (auto &item: cc2v_map) {
+                os << "\n\t"
+                    << "subgraph cluster" << item.first << " {";
+                for (int v: item.second) {
+                    os << "\n\t\t" << v << ";";
+                }
+                os << "\n\t}";
+            }
+            os << "\n";
+            return os.str();
+        };
+
+    // show dot
+    cout << "-----------------show dot of Graph--------------------\n";
+    GraphvizWriter::writeDot(graph, attributes);
+    cout << endl;
+    GraphvizWriter::writeDotFile("sample_undigraph_cc2.dot", graph, attributes);
+
+    return 0;
+}
+
